ebf5d901584347f43ff151faf73945b32f15e49e
nta2112 <ann249955@gmail.com>
2026-07-10 18:36:04 +0700

    sửa lỗi ablaition 4 (all_attr)

--- configs/abl/ip102_t1_attr_sel.py
+++ configs/abl/ip102_t1_attr_sel.py
@@ -3,6 +3,7 @@ _base_ = './ip102_t1_all_attr.py'
 model = dict(
     bbox_head=dict(
         select_all_attr=False,
-        selected_att_path='data/IP102/selected_att_embeddings.pth'
+        selected_att_path='data/IP102/selected_att_embeddings.pth',
+        attr_sel_for_known_only=True,
     )
 )
