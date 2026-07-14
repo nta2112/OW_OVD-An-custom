f31c71e974efde26c8459e101aad38a24af6227a
nta2112 <ann249955@gmail.com>
2026-07-14 23:57:41 +0700

    sửa lại abaltion 4

--- configs/abl/ip102_t1_attr_sel.py
+++ configs/abl/ip102_t1_attr_sel.py
@@ -1,10 +1,10 @@
-_base_ = './ip102_t1_all_attr.py'
+_base_ = './ip102_t1_all_attr_ood.py'
 
 model = dict(
     bbox_head=dict(
         select_all_attr=False,
         selected_att_path='data/IP102/selected_att_embeddings.pth',
-        attr_sel_for_known_only=True,
+        attr_sel_for_known_only=False,
         use_top_k_att=False,
         use_ood_gate=True
     )
