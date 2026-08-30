3eff4a77a7e41380817e4568e7b2a9f847dbbc0b
Nguyen Truong An <ann249955@gmail.com>
2026-08-30 23:18:26 +0700

    fix: duplicate retrieval configs in New_retrival folder for Kaggle compatibility

---
+++ New_retrival/ip102_t2_retrieval.py
@@ -0,0 +1,28 @@
+_base_ = '../configs/open_world/mowod/custom/ip102_t2.py'
+
+# Dynamically import our custom head from the local folder
+custom_imports = dict(
+    imports=['New_retrival.our_head_retrieval'],
+    allow_failed_imports=False
+)
+
+# Replace box head types to use our new retrieval head and module
+model = dict(
+    bbox_head=dict(
+        type='OurHeadRetrieval',
+        retrieval_dim=256,
+        loss_retrieval_weight=0.5,
+        triplet_margin=0.3,
+        head_module=dict(
+            type='OurHeadRetrievalModule',
+            retrieval_dim=256
+        )
+    )
+)
+
+# Training configurations: 1 epoch, batch size 16
+max_epochs = 1
+close_mosaic_epochs = 1
+train_batch_size_per_gpu = 16
+
+train_cfg = dict(max_epochs=max_epochs, val_interval=999)
